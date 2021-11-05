1067b5e2750e740f103de94163718752de4b07f2
Fabricio <contact@fabricioguardia.com>
2021-11-04 21:28:05 -0700

    added tab favicon

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,7 @@ function MyApp({ Component, pageProps }) {
     <Head>
       <title>Significard</title>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/significard.png"/>
       <link rel="preconnect" href="https://fonts.gstatic.com"></link>
       <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"></link>
       <link rel="preconnect" href="https://fonts.gstatic.com"></link>
