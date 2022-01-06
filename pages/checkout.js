ea32157120160d87abc4ac33cab58fcba219cc44
Fabricio <contact@fabricioguardia.com>
2022-01-06 15:45:52 -0800

    checkout update

--- pages/checkout.js
+++ pages/checkout.js
@@ -168,10 +168,10 @@ const Checkout = ({newUser}) => {
     if(recipientData.package_plan == 'good_deal')setPackagePrice(11.99)
 
     if(recipientData.package_plan === 'best deal')setTotal((6.99 * recipientData.package_quantity / 12))
-    if(recipientData.package_plan === 'best_deal')setTotal((6.99 * recipientData.package_quantity))
+    if(recipientData.package_plan === 'best_deal')setTotal((6.99 * recipientData.package_quantity / 12))
 
     if(recipientData.package_plan === 'better deal')setTotal((9.99 * recipientData.package_quantity / 12))
-    if(recipientData.package_plan === 'better_deal')setTotal((9.99 * recipientData.package_quantity))
+    if(recipientData.package_plan === 'better_deal')setTotal((9.99 * recipientData.package_quantity / 12))
     if(recipientData.package_plan === 'good deal')setTotal((11.99 * recipientData.package_quantity / 12))
     if(recipientData.package_plan === 'good_deal')setTotal((11.99 * recipientData.package_quantity / 12))
 
